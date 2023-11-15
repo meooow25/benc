7929bff61cd5f84991ca191af9d857191b7c0870
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2023-11-16 00:56:51 +0530

    Add parsers and encoders for Int* and Word* (#1)
    Parsers and encoders for Int64, Int32, Int16, Int8, Word64, Word32, Word16, Word8.

--- src/Data/Bencode/Util.hs
+++ src/Data/Bencode/Util.hs
@@ -1,42 +1,63 @@
 module Data.Bencode.Util
   ( readKnownNaturalAsInt
+  , readKnownNaturalAsInt64
   , readKnownNaturalAsWord
+  , readKnownNaturalAsWord64
   ) where
 
 import Data.Bits
+import Data.Int
+import Data.Word
 import qualified Data.ByteString as B
 
 -- | The input string must be an unsigned decimal integer with no extraneous
 -- leading zeros. Returns Nothing if the value is outside the bounds of an
 -- @Int@.
 readKnownNaturalAsInt :: Bool -> B.ByteString -> Maybe Int
-readKnownNaturalAsInt neg s = case B.uncons sr of
+readKnownNaturalAsInt = readInt maxIntLen
+  where
+    maxIntLen = case finiteBitSize (0 :: Int) of
+      32 -> 10
+      64 -> 19
+      _  -> error "unsupported word size"
+
+-- | Similar to 'readKnownNaturalAsInt', for 'Int64'.
+readKnownNaturalAsInt64 :: Bool -> B.ByteString -> Maybe Int64
+readKnownNaturalAsInt64 = readInt 19
+
+readInt :: (Bounded a, Integral a) => Int -> Bool -> B.ByteString -> Maybe a
+readInt maxLen neg s = case B.uncons sr of
   Nothing -> Just $! if neg then -n else n
   Just (d,sr')
     | B.null sr'
     , let d' = fromIntegral d - 48
-    , n < iMaxDiv10 || n == iMaxDiv10 && d' <= (7 + fromEnum neg)
+    , n < iMaxDiv10 || n == iMaxDiv10 && d' <= (7 + fromIntegral (fromEnum neg))
                        -- last digit of maxBound = 7, minBound = 8
     -> Just $! if neg then -n * 10 - d' else n * 10 + d'
     | otherwise -> Nothing
   where
-    (sl,sr) = B.splitAt (maxIntLen - 1) s
+    (sl,sr) = B.splitAt (maxLen - 1) s
     n = B.foldl' (\acc d -> acc * 10 + fromIntegral d - 48) 0 sl
-
-iMaxDiv10 :: Int
-iMaxDiv10 = maxBound `div` 10
-
-maxIntLen :: Int
-maxIntLen = case finiteBitSize (0 :: Int) of
-  32 -> 10
-  64 -> 19
-  _  -> error "unsupported word size"
+    iMaxDiv10 = maxBound `div` 10
+{-# INLINE readInt #-}
 
 -- | The input string must be an unsigned decimal integer with no extraneous
 -- leading zeros. Returns Nothing if the value is outside the bounds of a
 -- @Word@.
 readKnownNaturalAsWord :: B.ByteString -> Maybe Word
-readKnownNaturalAsWord s = case B.uncons sr of
+readKnownNaturalAsWord = readWord maxWordLen
+  where
+    maxWordLen = case finiteBitSize (0 :: Word) of
+      32 -> 10
+      64 -> 20
+      _  -> error "unsupported word size"
+
+-- | Similar to 'readKnownNaturalAsWord', for 'Word64'.
+readKnownNaturalAsWord64 :: B.ByteString -> Maybe Word64
+readKnownNaturalAsWord64 = readWord 20
+
+readWord :: (Bounded a, Integral a) => Int -> B.ByteString -> Maybe a
+readWord maxLen s = case B.uncons sr of
   Nothing -> Just $! n
   Just (d,sr')
     | B.null sr'
@@ -45,14 +66,7 @@ readKnownNaturalAsWord s = case B.uncons sr of
     -> Just $! n * 10 + d'
     | otherwise -> Nothing
   where
-    (sl,sr) = B.splitAt (maxWordLen - 1) s
+    (sl,sr) = B.splitAt (maxLen - 1) s
     n = B.foldl' (\acc d -> acc * 10 + fromIntegral d - 48) 0 sl
-
-wMaxDiv10 :: Word
-wMaxDiv10 = maxBound `div` 10
-
-maxWordLen :: Int
-maxWordLen = case finiteBitSize (0 :: Word) of
-  32 -> 10
-  64 -> 20
-  _  -> error "unsupported word size"
+    wMaxDiv10 = maxBound `div` 10
+{-# INLINE readWord #-}
